289450b1c41f2c96562f8fe0cf14dfe799a961b8
yusuke morizumi <y.morizumi@m.bokublock.jp>
2021-12-15 01:03:24 +0900

    Day 5 complete.

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Day2
 import Day3
 import Day4
 import Day5
+import qualified Day5
 
 main :: IO ()
-main = Day5.solve
+main = Day5.solve Day5.predicate'
--- src/Day5.hs
+++ src/Day5.hs
@@ -17,23 +17,34 @@ isHorizontal p q = _y p == _y q
 isVertical :: P -> P -> Bool
 isVertical p q = _x p == _x q
 
+isDiagonal :: P -> P -> Bool
+isDiagonal p q = abs (_x p - _x q) == abs (_y p - _y q)
+
 pointsOnLine :: P -> P -> [P]
 pointsOnLine p q
   | _x p == _x q = [P {_x = _x p, _y = y} | y <- [(min (_y p) (_y q)) .. (max (_y p) (_y q))]]
   | _y p == _y q = [P {_x = x, _y = _y p} | x <- [(min (_x p) (_x q)) .. (max (_x p) (_x q))]]
-  | otherwise = []
+  | _x p < _x q = zipWith (\x y -> P {_x = x, _y = y}) [(_x p) .. (_x q)] (iterate (\y -> if _y p > _y q then y - 1 else y + 1) (_y p))
+  | _x p > _x q = zipWith (\x y -> P {_x = x, _y = y}) [(_x q) .. (_x p)] (iterate (\y -> if _y q > _y p then y - 1 else y + 1) (_y q))
+  | otherwise = error "never reach here"
 
 splitOnArrow :: String -> (String, String)
 splitOnArrow s = let [p, q] = splitOn " -> " s in (p, q)
 
-makePoints :: [(String, String)] -> [P]
-makePoints spairs = do
+makePoints :: (P -> P -> Bool) -> [(String, String)] -> [P]
+makePoints predicate spairs = do
   (p1, p2) <- (\pair -> (makePoint $ fst pair, makePoint $ snd pair)) <$> spairs
-  if isVertical p1 p2 || isHorizontal p1 p2 then pointsOnLine p1 p2 else []
+  if predicate p1 p2 then pointsOnLine p1 p2 else []
 
-solve = do
+predicate :: P -> P -> Bool
+predicate p1 p2 = isVertical p1 p2 || isHorizontal p1 p2
+
+predicate' :: P -> P -> Bool
+predicate' p1 p2 = isVertical p1 p2 || isHorizontal p1 p2 || isDiagonal p1 p2
+
+solve predicate = do
   pairs <- fmap splitOnArrow . lines <$> getContents
-  let points = makePoints pairs
+  let points = makePoints predicate pairs
   let m = Prelude.filter (\(_, c) -> c > 1) (M.toList . go $ points)
   print $ length m
 
